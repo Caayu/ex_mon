6534d3bb33c57898c79fa9e04dfbbcc8d779500b
caayu <henrycay@outlook.com>
2020-10-14 20:16:27 -0300

    update credo

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ExMon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5.0-rc.2", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
